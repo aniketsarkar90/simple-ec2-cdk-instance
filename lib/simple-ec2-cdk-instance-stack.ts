8f6377bbdeba298032d221dfe23fe15d5b4f0128
Aniket Sarkar <aniketsarkar90@gmail.com>
2021-08-11 00:12:41 -0400

    installing cdk dependencies

--- lib/simple-ec2-cdk-instance-stack.ts
+++ lib/simple-ec2-cdk-instance-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
 
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as cdk from '@aws-cdk/aws-iam';
+
 export class SimpleEc2CdkInstanceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    //create a vpc where we'll launch the instance
+
+    //a security group
+
+    //role for the instance
+
+    //ec2 
   }
 }
