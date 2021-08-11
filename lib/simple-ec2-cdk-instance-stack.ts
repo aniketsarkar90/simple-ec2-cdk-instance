ec40a5493f995332f75c0a23afc628eae31a4123
Aniket Sarkar <aniketsarkar90@gmail.com>
2021-08-11 00:43:43 -0400

    updated infra
    added configurations for vpc, security group, role, and ec2

--- lib/simple-ec2-cdk-instance-stack.ts
+++ lib/simple-ec2-cdk-instance-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 
 import * as ec2 from '@aws-cdk/aws-ec2';
-import * as cdk from '@aws-cdk/aws-iam';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class SimpleEc2CdkInstanceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,11 +10,68 @@ export class SimpleEc2CdkInstanceStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     //create a vpc where we'll launch the instance
+    // Get the default VPC. This is the network where your instance will be provisioned
+    // All activated regions in AWS have a default vpc. 
+    // You can create your own of course as well. https://aws.amazon.com/vpc/
+    const vpc = new ec2.Vpc(this, 'simple-ec2-cdk-instance-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 0,
+      subnetConfiguration:[
+        {name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC},
+      ],
+    });
 
-    //a security group
+    //security group for the instance 
+    // A security group acts as a virtual firewall for your instance to control inbound and outbound traffic.
+    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+      vpc,
+      allowAllOutbound: true, // will let your instance send outboud traffic
+      description: 'security group for any common web server',
+    });
 
-    //role for the instance
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
 
-    //ec2 
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    //role for the ec2 instance
+    // You can attach permissions to a role and determine what your instance can or can not do
+    const webserverRole = new iam.Role(this, 'webserver-role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'), //As an example, we have attached a managed policy that grants S3 read access to the role.
+      ],
+    });
+
+    //creating the ec2 instance
+    const ec2Instance = new ec2.Instance(this, 'simple-ec2-instance', {
+      vpc, 
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      role: webserverRole,
+      securityGroup: webserverSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName: 'MyTestKeyPair',
+    });
   }
 }
